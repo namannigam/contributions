de179d751c25cdaf8895703b805196520f96cfc2
Naman <naman.nigam@nobroker.in>
2012-03-11 22:15:23 +0530

    add code in files types: ,.prefs,.xml,.java,.properties,.MF,.ftl,.jsp,.css,.js remove code in files types:

--- content.java
+++ content.java
@@ -9675,5 +9675,6 @@ public class Clwyfd {
         System.out.println("prhjp");
         System.out.println("cmnku");
         System.out.println("gvson");
+        System.out.println("gauxr");
     }
 }
