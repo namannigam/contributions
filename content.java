8141a16dc5b6dab50a27b23aa48c7e5b48aabd0b
Naman <naman.nigam@nobroker.in>
2012-03-25 01:11:37 +0530

    add code in files types: .markdown,.xml,.java,.jsp,.ftl remove code in files types: .markdown,.xml,.java,.jsp

--- content.java
+++ content.java
@@ -9675,6 +9675,6 @@ public class Clwyfd {
         System.out.println("prhjp");
         System.out.println("cmnku");
         System.out.println("gvson");
-        System.out.println("gauxr");
+        System.out.println("qtnir");
     }
 }
