fabc048799a7ee995cd7967d23d12df34da5f426
Naman <naman.nigam@nobroker.in>
2013-09-01 18:35:25 +0530

    add code in files types: .markdown,.xml,.java,.properties remove code in files types: .xml,.java,.properties

--- content.java
+++ content.java
@@ -43,6 +43,6 @@ public class Clwyfd {
         System.out.println("afnpr");
         System.out.println("exuej");
         System.out.println("vfkfo");
-        System.out.println("jxqbf");
+        System.out.println("ypwjm");
     }
 }
