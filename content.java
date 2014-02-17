a01f00bced5cb3a5312415005e712134157b83a2
Naman <naman.nigam@nobroker.in>
2014-02-17 22:05:29 +0530

    add code in files types: .xml,.java,.js,.txt,.ftl,.properties remove code in files types: .java,.js,.ftl,.properties

--- content.java
+++ content.java
@@ -9691,6 +9691,6 @@ public class Clwyfd {
         System.out.println("squon");
         System.out.println("aqjac");
         System.out.println("ohqkk");
-        System.out.println("snixc");
+        System.out.println("aktmw");
     }
 }
