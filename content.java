6cba833835343ab97c8dc4ab68f91d9ba20437f1
Naman <naman.nigam@nobroker.in>
2015-04-06 15:21:24 +0530

    add code in files types: ,.xml,.java,.css,.svg,.js,.ftl,.properties remove code in files types: .java,.css,.js,.ftl,.xml,.properties

--- content.java
+++ content.java
@@ -4927,6 +4927,6 @@ public class Clwyfd {
         System.out.println("jnhgp");
         System.out.println("cnfxx");
         System.out.println("kivcw");
-        System.out.println("tbjdx");
+        System.out.println("psvns");
     }
 }
