654790bb805b3213da5c83c970ad037212c9754a
Naman <naman.nigam@nobroker.in>
2015-05-11 11:56:21 +0530

    add code in files types: .hbs,.js,.css,.map,.scss remove code in files types: .hbs,.js,.css,.map,.scss

--- content.java
+++ content.java
@@ -4938,6 +4938,6 @@ public class Clwyfd {
         System.out.println("btukd");
         System.out.println("kqjkp");
         System.out.println("byvvg");
-        System.out.println("rcmik");
+        System.out.println("xafuh");
     }
 }
