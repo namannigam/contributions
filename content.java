e7d02e1a985374bed067050ba30c9695c5d4e700
Naman <naman.nigam@nobroker.in>
2015-06-21 01:35:06 +0530

    add code in files types: .xml,.java,.properties remove code in files types: .java,.properties

--- content.java
+++ content.java
@@ -9770,6 +9770,6 @@ public class Clwyfd {
         System.out.println("xqktv");
         System.out.println("fiibj");
         System.out.println("feknd");
-        System.out.println("dspwd");
+        System.out.println("rsies");
     }
 }
