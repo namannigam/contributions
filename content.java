8b9793ee5883fdaafa41eba10b8c672abc168f3f
Naman <naman.nigam@nobroker.in>
2015-07-29 11:38:56 +0530

    add code in files types: .hbs,.js remove code in files types: .hbs,.js

--- content.java
+++ content.java
@@ -9803,6 +9803,6 @@ public class Clwyfd {
         System.out.println("lkcuw");
         System.out.println("wrrqv");
         System.out.println("uyoek");
-        System.out.println("tqtcp");
+        System.out.println("uawap");
     }
 }
