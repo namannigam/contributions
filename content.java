0bcaef20dd58aa562efb0036b3348369761505d3
Naman <naman.nigam@nobroker.in>
2015-08-05 20:40:58 +0530

    add code in files types: .groovy,.java,.js,.hbs,.scss,.html,.css,.json,.txt,.ftl,.xml remove code in files types: .java,.js,.hbs,.scss,.html,.json,.txt,.ftl

--- content.java
+++ content.java
@@ -255,6 +255,6 @@ public class Clwyfd {
         System.out.println("boche");
         System.out.println("lheqd");
         System.out.println("vgaaq");
-        System.out.println("oybgm");
+        System.out.println("pgvin");
     }
 }
