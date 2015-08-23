db7fd4a1d89f9f22a739de79f8231bde0f5ffbc6
Naman <naman.nigam@nobroker.in>
2015-08-23 18:03:41 +0530

    add code in files types: .sql,.java,.js,.ftl remove code in files types: .sql,.java,.js,.ftl

--- content.java
+++ content.java
@@ -5012,6 +5012,6 @@ public class Clwyfd {
         System.out.println("bbnrs");
         System.out.println("kkxaj");
         System.out.println("lrjwl");
-        System.out.println("krkxv");
+        System.out.println("glrrq");
     }
 }
