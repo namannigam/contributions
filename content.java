e9317f72269e7c62898bd4005adbc3bf5ffefd19
Naman <naman.nigam@nobroker.in>
2015-09-08 13:31:20 +0530

    add code in files types: .sql,.java,.css,.map,.scss,.js,.ftl,.properties remove code in files types: .sql,.css,.map,.scss,.js,.ftl,.properties

--- content.java
+++ content.java
@@ -9827,5 +9827,6 @@ public class Clwyfd {
         System.out.println("ugrnp");
         System.out.println("ccnhh");
         System.out.println("pehvs");
+        System.out.println("auhxh");
     }
 }
--- content.sql
+++ content.sql
@@ -3623,4 +3623,4 @@ SELECT * from prowo;
 SELECT * from onxlj;
 SELECT * from ngwio;
 SELECT * from fcsjq;
-SELECT * from kprqw;
+SELECT * from wqhkt;
