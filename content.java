1c0a6b5f4346a105cc137af3eefc235cefdc1508
Naman <naman.nigam@nobroker.in>
2016-01-18 11:30:08 +0530

    add code in files types: .sql,.java,.hbs,.js,.scss,.ftl remove code in files types: .java,.hbs,.js,.scss,.ftl

--- content.java
+++ content.java
@@ -9939,6 +9939,6 @@ public class Clwyfd {
         System.out.println("poynu");
         System.out.println("rejrm");
         System.out.println("jfdbs");
-        System.out.println("gphxn");
+        System.out.println("solem");
     }
 }
--- content.sql
+++ content.sql
@@ -3711,3 +3711,4 @@ SELECT * from eeevu;
 SELECT * from kofkb;
 SELECT * from prfqo;
 SELECT * from vgdbq;
+SELECT * from eamqh;
