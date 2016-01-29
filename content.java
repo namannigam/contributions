d7d0a5fcf62c026b19667416f5b45d5cfdbe10c1
Naman <naman.nigam@nobroker.in>
2016-01-29 12:32:13 +0530

    add code in files types: .sh,.xml,.java,.css,.scss,.js,.ftl remove code in files types: .sh,.xml,.java,.css,.scss,.js,.ftl

--- content.java
+++ content.java
@@ -5144,6 +5144,6 @@ public class Clwyfd {
         System.out.println("ouhts");
         System.out.println("hsqqg");
         System.out.println("cqsiy");
-        System.out.println("rerhe");
+        System.out.println("pupda");
     }
 }
