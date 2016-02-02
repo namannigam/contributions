0236bc0476ac764b0dcc2dbb983948c9a6a797a0
Naman <naman.nigam@nobroker.in>
2016-02-02 11:36:56 +0530

    add code in files types: .sh,.groovy,.java,.sql,.hbs,.js,.css,.scss,.ftl remove code in files types: .groovy,.sql,.java,.hbs,.js,.css,.scss,.ftl

--- content.java
+++ content.java
@@ -403,6 +403,6 @@ public class Clwyfd {
         System.out.println("ghslt");
         System.out.println("qpake");
         System.out.println("isxru");
-        System.out.println("dmynq");
+        System.out.println("ojche");
     }
 }
