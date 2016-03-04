b9a5f957b7cf15b3fe6615f2f4887c63caa4b502
Naman <naman.nigam@nobroker.in>
2016-03-04 12:29:29 +0530

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -5167,6 +5167,6 @@ public class Clwyfd {
         System.out.println("vmhed");
         System.out.println("fvuik");
         System.out.println("hlrvs");
-        System.out.println("nmdyv");
+        System.out.println("gpmbw");
     }
 }
--- content.sql
+++ content.sql
@@ -1952,4 +1952,4 @@ SELECT * from sanac;
 SELECT * from wrged;
 SELECT * from tpeje;
 SELECT * from yvkso;
-SELECT * from moiox;
+SELECT * from lyrud;
