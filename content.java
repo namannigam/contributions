17524bfdfb8d7706467a32f2654821715825e16d
Naman <naman.nigam@nobroker.in>
2016-03-15 02:45:26 +0530

    add code in files types: .js,.scss remove code in files types: .js

--- content.java
+++ content.java
@@ -9986,5 +9986,7 @@ public class Clwyfd {
         System.out.println("ddqgf");
         System.out.println("wtmyl");
         System.out.println("xtwfi");
+        System.out.println("evvec");
+        System.out.println("tfsyh");
     }
 }
--- content.sql
+++ content.sql
@@ -3740,4 +3740,6 @@ SELECT * from mssgy;
 SELECT * from wclsf;
 SELECT * from daqso;
 SELECT * from rrudu;
-SELECT * from eiabk;
+SELECT * from rdgun;
+SELECT * from woiom;
+SELECT * from njdsu;
