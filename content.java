3584ffb0622ccd0e5a19ce4e48373526dc13ba5f
Naman <naman.nigam@nobroker.in>
2016-03-16 12:38:51 +0530

    add code in files types: .py,,.sql,.java,.html,.js,.hbs,.scss,.json,.css,.ftl remove code in files types: .py,,.sql,.java,.html,.js,.hbs,.scss,.json,.css,.ftl

--- content.java
+++ content.java
@@ -9991,6 +9991,6 @@ public class Clwyfd {
         System.out.println("liskb");
         System.out.println("fftyh");
         System.out.println("fvybg");
-        System.out.println("hrwvf");
+        System.out.println("xbnve");
     }
 }
--- content.sql
+++ content.sql
@@ -3743,4 +3743,4 @@ SELECT * from rrudu;
 SELECT * from rdgun;
 SELECT * from woiom;
 SELECT * from njdsu;
-SELECT * from nmyrm;
+SELECT * from vwntg;
