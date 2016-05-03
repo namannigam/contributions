90136035350be599ce1fc552759ae4364ba7aa9e
Naman <naman.nigam@nobroker.in>
2016-05-03 14:06:15 +0530

    add code in files types: .js,.scss remove code in files types: .js,.scss

--- content.java
+++ content.java
@@ -10045,6 +10045,6 @@ public class Clwyfd {
         System.out.println("fxjoq");
         System.out.println("ubslf");
         System.out.println("irkio");
-        System.out.println("crldd");
+        System.out.println("dytfv");
     }
 }
