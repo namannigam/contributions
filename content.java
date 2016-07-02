4f3f2b22ed2402ae72f7ca10f3d43cf5901c5852
Naman <naman.nigam@nobroker.in>
2016-07-02 13:34:59 +0530

    add code in files types: .txt,.sql,.java,.js,.hbs,.scss,.css,.ftl remove code in files types: .txt,.sql,.java,.js,.hbs,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -566,6 +566,6 @@ public class Clwyfd {
         System.out.println("wojhs");
         System.out.println("xuwgb");
         System.out.println("llipf");
-        System.out.println("njynn");
+        System.out.println("hyqyp");
     }
 }
--- content.sql
+++ content.sql
@@ -314,4 +314,4 @@ SELECT * from dcnyb;
 SELECT * from pwpsf;
 SELECT * from rmyth;
 SELECT * from wyqda;
-SELECT * from awtwf;
+SELECT * from nyffx;
