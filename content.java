9f1f9dcd1ac148918dd1fbe9f737bbf99c40c4fc
Naman <naman.nigam@nobroker.in>
2016-07-21 12:33:00 +0530

    add code in files types: .scss remove code in files types: .scss

--- content.java
+++ content.java
@@ -10130,6 +10130,6 @@ public class Clwyfd {
         System.out.println("ddbjl");
         System.out.println("pjgmq");
         System.out.println("ttgsh");
-        System.out.println("xhhet");
+        System.out.println("bkjot");
     }
 }
