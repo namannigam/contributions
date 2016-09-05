c1fa9fe1c0fabccb5bc1dc50d9a4f230a77926c4
Naman <naman.nigam@nobroker.in>
2016-09-05 13:18:12 +0530

    add code in files types: .avsc,.xml,.java,.html,.js,.hbs,.scss,.ftl,.git-id remove code in files types: .xml,.java,.html,.js,.hbs,.scss,.ftl

--- content.java
+++ content.java
@@ -5401,6 +5401,6 @@ public class Clwyfd {
         System.out.println("rrrjh");
         System.out.println("sncxd");
         System.out.println("lnllc");
-        System.out.println("bxair");
+        System.out.println("ygljk");
     }
 }
