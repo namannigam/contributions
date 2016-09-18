fa662579ebb2e14b801f437579e3b11c798480cc
Naman <naman.nigam@nobroker.in>
2016-09-18 19:30:45 +0530

    add code in files types: .avsc,.java,.git-id remove code in files types: ,.avsc,.java

--- content.java
+++ content.java
@@ -5432,6 +5432,6 @@ public class Clwyfd {
         System.out.println("gjido");
         System.out.println("vkyyi");
         System.out.println("cugov");
-        System.out.println("wixty");
+        System.out.println("lawna");
     }
 }
