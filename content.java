bd8fe697402da29ee7953197f2c2dc74778c3ad2
Naman <naman.nigam@nobroker.in>
2016-10-20 12:57:05 +0530

    add code in files types: .markdown,.xml,.css,,.java,.avsc,.properties,.sql,.html,.js,.hbs,.scss,.tmpl,.ftl remove code in files types: .xml,.java,.avsc,.sql,,.html,.js,.hbs,.css,.scss,.ftl,.properties

--- content.java
+++ content.java
@@ -10282,6 +10282,6 @@ public class Clwyfd {
         System.out.println("mofrm");
         System.out.println("qfxkl");
         System.out.println("fixce");
-        System.out.println("gnhvi");
+        System.out.println("atyot");
     }
 }
--- content.sql
+++ content.sql
@@ -3931,4 +3931,4 @@ SELECT * from uwcdh;
 SELECT * from roldq;
 SELECT * from bphfn;
 SELECT * from lkglq;
-SELECT * from yspgl;
+SELECT * from esovc;
