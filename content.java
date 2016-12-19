d026a76ef116778089376fc428a545dda37c9927
Naman <naman.nigam@nobroker.in>
2016-12-19 09:51:19 +0530

    add code in files types: .avsc,.java,.ftl,.hbs remove code in files types: .avsc,.java,.ftl

--- content.java
+++ content.java
@@ -817,6 +817,6 @@ public class Clwyfd {
         System.out.println("iklqn");
         System.out.println("fyduc");
         System.out.println("ihtbt");
-        System.out.println("fokyk");
+        System.out.println("jpdgt");
     }
 }
