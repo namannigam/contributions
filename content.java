10970d09e4c436420249d076c67804b683b504a3
Naman <naman.nigam@nobroker.in>
2017-04-06 12:15:26 +0530

    add code in files types: .xml,.avsc,.sh,.groovy,.txt,.java,.sql,.html,.js,.hbs,.scss,.css,.ftl remove code in files types: .xml,.sh,.groovy,.java,.sql,.html,.js,.hbs,.css,.ftl

--- content.java
+++ content.java
@@ -5761,6 +5761,6 @@ public class Clwyfd {
         System.out.println("ejfuw");
         System.out.println("gppxi");
         System.out.println("gwwtp");
-        System.out.println("kcdle");
+        System.out.println("ydyjn");
     }
 }
--- content.sql
+++ content.sql
@@ -2301,4 +2301,4 @@ SELECT * from xbnyd;
 SELECT * from qddmn;
 SELECT * from quknw;
 SELECT * from iiiyu;
-SELECT * from icyda;
+SELECT * from jtxqv;
