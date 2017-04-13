729eccedcb179fdf6e91fe0d3f628057e8ce7492
Naman <naman.nigam@nobroker.in>
2017-04-13 12:23:16 +0530

    add code in files types: .properties,.sql,.java remove code in files types: .properties,.sql

--- content.java
+++ content.java
@@ -5768,5 +5768,6 @@ public class Clwyfd {
         System.out.println("gkacc");
         System.out.println("orlqh");
         System.out.println("fjorn");
+        System.out.println("krcpl");
     }
 }
--- content.sql
+++ content.sql
@@ -2309,4 +2309,4 @@ SELECT * from ikdxd;
 SELECT * from kguqc;
 SELECT * from dvcvg;
 SELECT * from tgyrj;
-SELECT * from ygtmp;
+SELECT * from ffuca;
