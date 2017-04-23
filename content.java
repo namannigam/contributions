0afa1affce860185270d99f798da935f240fa214
Naman <naman.nigam@nobroker.in>
2017-04-23 23:05:59 +0530

    add code in files types: .avsc,.java remove code in files types: .java

--- content.java
+++ content.java
@@ -5780,6 +5780,6 @@ public class Clwyfd {
         System.out.println("gueif");
         System.out.println("juhvb");
         System.out.println("avayp");
-        System.out.println("jqlix");
+        System.out.println("crhdj");
     }
 }
