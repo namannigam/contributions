9f0c41e13096a11385c6db2f93972798e9336c75
Naman <naman.nigam@nobroker.in>
2017-04-29 16:48:41 +0530

    add code in files types: .py,.xml,.java,,.properties,.sql,.hbs,.js,.scss,.css,.ftl remove code in files types: .py,.xml,.java,.properties,.sql,.hbs,.js,.css,.scss,.ftl

--- content.java
+++ content.java
@@ -10595,6 +10595,6 @@ public class Clwyfd {
         System.out.println("ugeyw");
         System.out.println("ujmla");
         System.out.println("efbnk");
-        System.out.println("bljir");
+        System.out.println("pyjap");
     }
 }
