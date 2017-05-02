83ec991a7e38483e54cc79c637aff490f01f0cf3
Naman <naman.nigam@nobroker.in>
2017-05-02 12:41:47 +0530

    add code in files types: .avsc,.py,.txt,.xml,.java,,.properties,.sh,.sql,.js,.hbs,.scss,.css,.ftl remove code in files types: .py,.xml,.java,.properties,.sql,.js,.hbs,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -5791,6 +5791,6 @@ public class Clwyfd {
         System.out.println("sejvs");
         System.out.println("dbdpo");
         System.out.println("rgait");
-        System.out.println("erslh");
+        System.out.println("vkcnb");
     }
 }
