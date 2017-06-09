cad2c962aa8f7642667497638518a42b8f378764
Naman <naman.nigam@nobroker.in>
2017-06-09 11:35:39 +0530

    add code in files types: .avsc,.java,.sql,.css,.js,.ftl remove code in files types: .java,.sql,.js,.ftl

--- content.java
+++ content.java
@@ -10656,6 +10656,6 @@ public class Clwyfd {
         System.out.println("nwics");
         System.out.println("kchsl");
         System.out.println("jsoij");
-        System.out.println("lhbpk");
+        System.out.println("wuwql");
     }
 }
--- content.sql
+++ content.sql
@@ -4145,4 +4145,4 @@ SELECT * from jrieu;
 SELECT * from rjeph;
 SELECT * from bvvmv;
 SELECT * from iahvk;
-SELECT * from mygcw;
+SELECT * from wqrom;
