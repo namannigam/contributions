1c3e648d396a438c102781028b62168eec9f4e36
Naman <naman.nigam@nobroker.in>
2017-07-11 12:08:10 +0530

    add code in files types: .bck,.xml,.avsc,,.py,.java,.sql,.properties,.json,.html,.js,.hbs,.scss,.css,.ftl remove code in files types: .bck,.xml,.avsc,,.py,.java,.sql,.properties,.json,.html,.js,.hbs,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -10760,6 +10760,6 @@ public class Clwyfd {
         System.out.println("rldtl");
         System.out.println("lymeq");
         System.out.println("lwueu");
-        System.out.println("dwaea");
+        System.out.println("xrpqv");
     }
 }
