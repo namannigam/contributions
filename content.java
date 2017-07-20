0659b2ac368d7403fa367363c3835eacef9770e9
Naman <naman.nigam@nobroker.in>
2017-07-20 12:09:34 +0530

    add code in files types: ,.xml,.groovy,.java,.sh,.sql,.html,.js,.hbs,.scss,.css,.ftl remove code in files types: .bck,.xml,,.groovy,.java,.sql,.html,.js,.hbs,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -5967,6 +5967,6 @@ public class Clwyfd {
         System.out.println("mmulc");
         System.out.println("frvpu");
         System.out.println("xtlth");
-        System.out.println("qtued");
+        System.out.println("jjjjd");
     }
 }
