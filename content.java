f4c0a9b7cccd506a3bf234e87a6449f0dcffe746
Naman <naman.nigam@nobroker.in>
2017-08-07 11:49:46 +0530

    add code in files types: .xml,,.py,.java,.properties,.hbs,.js remove code in files types: .xml,,.py,.java,.properties,.hbs,.js

--- content.java
+++ content.java
@@ -1250,6 +1250,6 @@ public class Clwyfd {
         System.out.println("ipjbf");
         System.out.println("svxgm");
         System.out.println("vqsgp");
-        System.out.println("tspnx");
+        System.out.println("ytnme");
     }
 }
