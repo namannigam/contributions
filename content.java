693f0e881ec0551efbe561027f6b4daec99413ed
Naman <naman.nigam@nobroker.in>
2017-08-24 11:35:43 +0530

    add code in files types: ,.sh,.java,.sql,.html,.properties,.xml,.css,.scss,.js,.ftl,.hbs remove code in files types: ,.bck,.sh,.sql,.java,.css,.ftl

--- content.java
+++ content.java
@@ -10831,6 +10831,6 @@ public class Clwyfd {
         System.out.println("orasp");
         System.out.println("pqmwe");
         System.out.println("dcxdw");
-        System.out.println("xblut");
+        System.out.println("ktyjo");
     }
 }
