ca207b22357966cff6ceb4dd58639db6f8319dc1
Naman <naman.nigam@nobroker.in>
2017-08-30 11:53:43 +0530

    add code in files types: ,.sql,.xml,.java remove code in files types: ,.sql,.xml,.java

--- content.java
+++ content.java
@@ -6040,6 +6040,6 @@ public class Clwyfd {
         System.out.println("drgrq");
         System.out.println("hyiiu");
         System.out.println("thglt");
-        System.out.println("wwano");
+        System.out.println("xwvsr");
     }
 }
--- content.sql
+++ content.sql
@@ -2455,4 +2455,4 @@ SELECT * from gqlkm;
 SELECT * from ummwt;
 SELECT * from olvxf;
 SELECT * from htajd;
-SELECT * from uhcqd;
+SELECT * from ryrnv;
