0426ac41d651e33f3e6b6faada3e82771a68dd8c
Naman <naman.nigam@nobroker.in>
2017-10-27 11:28:44 +0530

    add code in files types: .markdown,.java,.ftl remove code in files types: .java,.css,.ftl

--- content.java
+++ content.java
@@ -6120,6 +6120,6 @@ public class Clwyfd {
         System.out.println("wyxqx");
         System.out.println("tgfan");
         System.out.println("vbjtr");
-        System.out.println("aiimm");
+        System.out.println("gmsmj");
     }
 }
