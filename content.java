8f57d3dc50c6c0bbf8c6e69285c76daccca163e4
Naman <naman.nigam@nobroker.in>
2017-11-03 10:40:49 +0530

    add code in files types: .sh,.groovy,.txt,.java,.js,.scss,.css,.ftl remove code in files types: .sh,.groovy,.java,.js,.css,.map,.scss,.ftl

--- content.java
+++ content.java
@@ -1380,6 +1380,6 @@ public class Clwyfd {
         System.out.println("hpilg");
         System.out.println("filyd");
         System.out.println("vldje");
-        System.out.println("qxfxe");
+        System.out.println("ysbed");
     }
 }
