a408c06739a00068b5bb4a11100875e880cfd37d
Naman <naman.nigam@nobroker.in>
2018-02-28 11:16:47 +0530

    add code in files types: ,.py,.sh,.sql,.git-id,.java,.properties,.hbs,.js,.scss,.ftl remove code in files types: ,.py,.sh,.sql,.git-id,.java,.properties,.hbs,.js,.scss,.ftl

--- content.java
+++ content.java
@@ -6270,6 +6270,6 @@ public class Clwyfd {
         System.out.println("abywr");
         System.out.println("scfbb");
         System.out.println("ioyvd");
-        System.out.println("pwiue");
+        System.out.println("nafiy");
     }
 }
--- content.sql
+++ content.sql
@@ -2625,4 +2625,4 @@ SELECT * from orxlk;
 SELECT * from xgkeb;
 SELECT * from ocbwx;
 SELECT * from gjjmk;
-SELECT * from foida;
+SELECT * from jvhjy;
