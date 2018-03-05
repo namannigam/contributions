3bb61f140ac35c7cb61aea2f30c08c2a7f9a3299
Naman <naman.nigam@nobroker.in>
2018-03-05 13:13:39 +0530

    add code in files types: .java,.js,.hbs remove code in files types: .java,.js,.hbs

--- content.java
+++ content.java
@@ -1530,6 +1530,6 @@ public class Clwyfd {
         System.out.println("kjpvk");
         System.out.println("hvywy");
         System.out.println("cybla");
-        System.out.println("jongn");
+        System.out.println("iqvhn");
     }
 }
