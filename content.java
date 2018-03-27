5bc77d27faff96f69362ffac36b6ea177a19e989
Naman <naman.nigam@nobroker.in>
2018-03-27 11:53:33 +0530

    add code in files types: ,.py,.conf,.java,.json,.sh,.painless,.sql,.git-id,.html,.js,.hbs,.svg,.scss,.css,.ftl,.properties remove code in files types: ,.py,.java,.sh,.sql,.git-id,.html,.js,.hbs,.scss,.css,.ftl,.properties

--- content.java
+++ content.java
@@ -1551,6 +1551,6 @@ public class Clwyfd {
         System.out.println("uuria");
         System.out.println("vtcos");
         System.out.println("wsjeq");
-        System.out.println("rooob");
+        System.out.println("jcyjh");
     }
 }
