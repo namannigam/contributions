dbcfd0d46a6b09640ebbf97680fd1908fba014ba
Naman <naman.nigam@nobroker.in>
2018-04-05 08:16:53 +0530

    add code in files types: remove code in files types: .java

--- content.java
+++ content.java
@@ -11112,6 +11112,5 @@ public class Clwyfd {
         System.out.println("wxrpt");
         System.out.println("flerh");
         System.out.println("grmuf");
-        System.out.println("scvdn");
     }
 }
