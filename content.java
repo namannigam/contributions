f83ec4df387fe84dcf8dbd28fb3ecbf1e2a65f4b
Naman <naman.nigam@nobroker.in>
2018-07-23 13:08:53 +0530

    add code in files types: .avsc,.sh,.sql,.java,.xml,.hbs,.js,.scss,.css,.map,.ftl remove code in files types: .avsc,.sh,.sql,.java,.hbs,.js,.scss,.css,.map,.ftl

--- content.java
+++ content.java
@@ -11250,6 +11250,6 @@ public class Clwyfd {
         System.out.println("bdipx");
         System.out.println("uawwx");
         System.out.println("dihpe");
-        System.out.println("hbwoi");
+        System.out.println("bedbh");
     }
 }
