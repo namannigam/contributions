feba294ae02c29821ac4c9b32bd22f376e567e01
Naman <naman.nigam@nobroker.in>
2018-08-24 13:42:19 +0530

    add code in files types: ,.sh,.java,.xml,.html,.js,.hbs,.scss,.css,.map,.ftl remove code in files types: ,.sh,.java,.xml,.js,.hbs,.scss,.css,.map,.ftl

--- content.java
+++ content.java
@@ -1782,6 +1782,6 @@ public class Clwyfd {
         System.out.println("bweke");
         System.out.println("kjkno");
         System.out.println("gltpx");
-        System.out.println("ypreh");
+        System.out.println("kkwpm");
     }
 }
