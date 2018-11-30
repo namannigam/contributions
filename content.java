ac367e7691cb25e10121e18240329203bb52a060
Naman <naman.nigam@nobroker.in>
2018-11-30 11:24:16 +0530

    add code in files types: ,.avsc,.sh,.json,.java,.sql,.git-id,.properties,.html,.js,.hbs,.css,.scss,.ftl remove code in files types: ,.sh,.json,.java,.sql,.properties,.html,.js,.hbs,.css,.scss,.ftl

--- content.java
+++ content.java
@@ -6744,6 +6744,6 @@ public class Clwyfd {
         System.out.println("gtyrj");
         System.out.println("opaxi");
         System.out.println("hbkrn");
-        System.out.println("qwivb");
+        System.out.println("egcha");
     }
 }
