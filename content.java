6d3cdb9d476a28166e4a3074927cf81419e11eee
Naman <naman.nigam@nobroker.in>
2018-12-07 11:24:59 +0530

    add code in files types: .git-id,.java,.js,.ftl remove code in files types: .git-id,.java,.js,.ftl

--- content.java
+++ content.java
@@ -11562,6 +11562,6 @@ public class Clwyfd {
         System.out.println("xahcx");
         System.out.println("cvden");
         System.out.println("ynqig");
-        System.out.println("wascd");
+        System.out.println("mrulp");
     }
 }
