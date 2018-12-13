8faf1577ae39d8988cdab4aea35dfda40d6cc7bd
Naman <naman.nigam@nobroker.in>
2018-12-13 11:13:26 +0530

    add code in files types: ,.avsc,.py,.log,.java,.painless,.git-id,.sql,.properties,.xml,.js,.hbs,.scss,.css,.ftl remove code in files types: ,.avsc,.py,.java,.painless,.git-id,.sql,.properties,.xml,.js,.hbs,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -2021,6 +2021,6 @@ public class Clwyfd {
         System.out.println("njabl");
         System.out.println("yyatg");
         System.out.println("ufulp");
-        System.out.println("boecq");
+        System.out.println("skxbi");
     }
 }
