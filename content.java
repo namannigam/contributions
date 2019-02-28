71c4de786658d219d5967ed8c4b4ea37933e98d5
Naman <naman.nigam@nobroker.in>
2019-02-28 13:28:38 +0530

    add code in files types: ,.sh,.properties,.xml,.java,.ftl remove code in files types: ,.sh,.properties,.xml,.java,.ftl

--- content.java
+++ content.java
@@ -6892,6 +6892,6 @@ public class Clwyfd {
         System.out.println("rexxk");
         System.out.println("ocxyo");
         System.out.println("dkkug");
-        System.out.println("ujnuc");
+        System.out.println("unvgv");
     }
 }
