41078433fb6c6885a2a1214a9cb7c04a21b13736
Naman <naman.nigam@nobroker.in>
2019-03-06 00:11:15 +0530

    add code in files types: .sh,.py,,.properties,.java,.xml remove code in files types: .py,,.sh,.java,.xml

--- content.java
+++ content.java
@@ -11712,6 +11712,6 @@ public class Clwyfd {
         System.out.println("ltyqo");
         System.out.println("smlat");
         System.out.println("ygods");
-        System.out.println("hqocs");
+        System.out.println("pfhfy");
     }
 }
