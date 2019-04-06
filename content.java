680862022370a5393254c4ca9073606b852895d7
Naman <naman.nigam@nobroker.in>
2019-04-06 14:08:58 +0530

    add code in files types: ,.avsc,.java,.html,.js,.hbs,.scss,.css,.ftl remove code in files types: ,.java,.hbs,.js,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -2220,6 +2220,6 @@ public class Clwyfd {
         System.out.println("gpjea");
         System.out.println("fmfsq");
         System.out.println("tskxe");
-        System.out.println("tynnf");
+        System.out.println("ghqqf");
     }
 }
