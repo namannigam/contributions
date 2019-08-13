851fa744704a84d194d13913e4a2c335dc7eaf8a
Naman <naman.nigam@nobroker.in>
2019-08-13 11:46:07 +0530

    add code in files types: .py,.conf,.java,.properties,.html remove code in files types: .conf,.java,.properties

--- content.java
+++ content.java
@@ -12035,6 +12035,6 @@ public class Clwyfd {
         System.out.println("hlevm");
         System.out.println("aoktl");
         System.out.println("fwycn");
-        System.out.println("iwexx");
+        System.out.println("iuist");
     }
 }
