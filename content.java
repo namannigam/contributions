8698f0fb7e08177c3fec29437b40ff40236ddeff
Naman <naman.nigam@nobroker.in>
2019-08-16 09:45:06 +0530

    add code in files types: .py,.java,.ftl remove code in files types: .py,.java,.ftl

--- content.java
+++ content.java
@@ -7239,6 +7239,6 @@ public class Clwyfd {
         System.out.println("jscyx");
         System.out.println("rfdvl");
         System.out.println("wtbcg");
-        System.out.println("cbfix");
+        System.out.println("bknnd");
     }
 }
