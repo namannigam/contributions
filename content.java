b2ae2eba5d516c97a8724bda8eb6a4c93fb4de81
Naman <naman.nigam@nobroker.in>
2019-10-30 15:49:02 +0530

    add code in files types: .sh,.json,.git-id,.sql,.java,.hbs,.js,.svg,.scss,.css,.ftl remove code in files types: .sh,.json,.git-id,.sql,.java,.hbs,.js,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -7358,6 +7358,6 @@ public class Clwyfd {
         System.out.println("rchdx");
         System.out.println("mnqly");
         System.out.println("ceadm");
-        System.out.println("nspxj");
+        System.out.println("dfmqg");
     }
 }
