f2b59d1adac90eca1cec5c4c05a150599bfd84e1
Naman <naman.nigam@nobroker.in>
2020-05-08 11:15:19 +0530

    add code in files types: .sh,.sql,.java,.properties,.xml,.js,.hbs,.scss,.css,.map,.svg,.ftl remove code in files types: .sh,.sql,.java,.properties,.xml,.js,.hbs,.scss,.css,.ftl

--- content.java
+++ content.java
@@ -7792,6 +7792,6 @@ public class Clwyfd {
         System.out.println("pwtua");
         System.out.println("qvrbv");
         System.out.println("fmnin");
-        System.out.println("wqnah");
+        System.out.println("qynaf");
     }
 }
