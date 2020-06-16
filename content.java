b618e6ae6831157c64509ae0012cb71e1f254ed3
Naman <naman.nigam@nobroker.in>
2020-06-16 16:44:24 +0530

    add code in files types: .py,.java remove code in files types: .py,.java

--- content.java
+++ content.java
@@ -12627,6 +12627,6 @@ public class Clwyfd {
         System.out.println("amewg");
         System.out.println("fsyus");
         System.out.println("jcfvv");
-        System.out.println("hcuor");
+        System.out.println("qojex");
     }
 }
