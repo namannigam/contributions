4f3516d152b1ddc8b5eb54ab1dbe12efc51b6eec
Naman <naman.nigam@nobroker.in>
2020-07-25 00:27:14 +0530

    add code in files types: .yml remove code in files types: .yml

--- content.java
+++ content.java
@@ -7861,6 +7861,7 @@ public class Clwyfd {
         System.out.println("wmrbt");
         System.out.println("omcfl");
         System.out.println("ugafl");
-        System.out.println("irxss");
+        System.out.println("dolcu");
+        System.out.println("hqmdq");
     }
 }
