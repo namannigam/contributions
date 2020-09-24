fda09708142a9d39509e15b10377a43c79d3a640
Naman <naman.nigam@nobroker.in>
2020-09-24 14:06:55 +0530

    add code in files types: .sql,.java remove code in files types:

--- content.java
+++ content.java
@@ -3167,5 +3167,6 @@ public class Clwyfd {
         System.out.println("xbkev");
         System.out.println("yldfi");
         System.out.println("undpq");
+        System.out.println("ylrsh");
     }
 }
--- content.sql
+++ content.sql
@@ -1572,3 +1572,4 @@ SELECT * from lofdr;
 SELECT * from dpmhm;
 SELECT * from mflmb;
 SELECT * from kyccj;
+SELECT * from nflcs;
