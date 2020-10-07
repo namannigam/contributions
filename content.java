b71b7631a0dfdb0a6e4940c399b2376ad756ebc5
Naman <naman.nigam@nobroker.in>
2020-10-08 02:30:53 +0530

    add code in files types: .avsc,.java,.js,.ftl remove code in files types: .java,.js,.ftl

--- content.java
+++ content.java
@@ -7925,6 +7925,6 @@ public class Clwyfd {
         System.out.println("yepxr");
         System.out.println("oboqq");
         System.out.println("dafra");
-        System.out.println("apwfv");
+        System.out.println("bjfou");
     }
 }
