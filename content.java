3ee1b9cb03b391db467411e22efd5589cacc04e7
Naman <naman.nigam@nobroker.in>
2021-03-19 12:56:17 +0530

    add code in files types: .sql,.java remove code in files types: .sql,.java

--- content.java
+++ content.java
@@ -8131,6 +8131,6 @@ public class Clwyfd {
         System.out.println("gkjsa");
         System.out.println("gbjvr");
         System.out.println("ixwos");
-        System.out.println("ypnrd");
+        System.out.println("afglr");
     }
 }
