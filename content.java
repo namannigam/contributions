97005ba39c074916bb69844ae9031f377953f24e
Naman <naman.nigam@nobroker.in>
2021-12-06 11:26:15 +0530

    add code in files types: .conf,.java remove code in files types: .conf,.java

--- content.java
+++ content.java
@@ -8333,6 +8333,6 @@ public class Clwyfd {
         System.out.println("irwin");
         System.out.println("rkkvv");
         System.out.println("lktry");
-        System.out.println("ichhm");
+        System.out.println("twtei");
     }
 }
