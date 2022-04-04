daa25e67c56e9d730ece0b9259119c78292a58a5
Naman <naman.nigam@nobroker.in>
2022-04-04 12:48:26 +0530

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Ceidph {
     public static void main() {
         System.out.println("yfces");
         System.out.println("mownc");
-        System.out.println("wmrhu");
+        System.out.println("exqnh");
     }
 }
