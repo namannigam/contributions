68e23e64f36aec0c791d3975d22d5e40eb1ebc6c
Naman <naman.nigam@nobroker.in>
2022-05-30 23:17:11 +0530

    add code in files types: .java,.js remove code in files types: .java,.js

--- content.java
+++ content.java
@@ -1,6 +1,6 @@
 public class Cswsmc {
     public static void main() {
         System.out.println("adkfu");
-        System.out.println("gcnmo");
+        System.out.println("gcofh");
     }
 }
--- content.js
+++ content.js
@@ -1,2 +1,2 @@
 console.log("llpte")
-console.log("avrvx")
+console.log("gqtpu")
