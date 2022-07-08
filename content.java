c52dd7af15121ae1453d691b0be398a69b232a63
Naman <naman.nigam@nobroker.in>
2022-07-08 15:31:50 +0530

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Coaveu {
     public static void main() {
         System.out.println("guuxn");
         System.out.println("povfa");
-        System.out.println("fhyhm");
+        System.out.println("iowri");
     }
 }
