32a7db7db3cb17fda442aa9ffa57b7c73f6f5f34
Naman <naman.nigam@nobroker.in>
2022-09-26 12:17:37 +0530

    add code in files types: .xml remove code in files types: ,.xml,.yaml,.sh,.java,.properties

--- content.java
+++ content.java
@@ -13360,6 +13360,5 @@ public class Clwyfd {
         System.out.println("ekano");
         System.out.println("ukiap");
         System.out.println("xvcbr");
-        System.out.println("kfdlm");
     }
 }
