d1811e9ebff0368b0561f430e99d141eed16fb6f
Naman <naman.nigam@nobroker.in>
2022-10-07 16:21:38 +0530

    add code in files types: .java,.sql,.properties remove code in files types: .java,.sql,.ftl

--- content.java
+++ content.java
@@ -8569,6 +8569,6 @@ public class Clwyfd {
         System.out.println("fkgux");
         System.out.println("fdlig");
         System.out.println("mbedh");
-        System.out.println("jsmdg");
+        System.out.println("xwbyd");
     }
 }
--- content.sql
+++ content.sql
@@ -3534,4 +3534,4 @@ SELECT * from hlldw;
 SELECT * from rlhnk;
 SELECT * from bewcs;
 SELECT * from rlnel;
-SELECT * from ombby;
+SELECT * from ffgpc;
