8a1689833ed313cc350953244d60a4baccd2ef66
Naman <naman.nigam@nobroker.in>
2022-11-25 11:28:52 +0530

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -8628,6 +8628,6 @@ public class Clwyfd {
         System.out.println("nejvc");
         System.out.println("pfgfa");
         System.out.println("bietm");
-        System.out.println("oinvq");
+        System.out.println("mignd");
     }
 }
