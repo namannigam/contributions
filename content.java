c63ee3aaad75bfd24b0de242397dad8445ebce23
Naman <naman.nigam@nobroker.in>
2023-01-11 12:09:00 +0530

    add code in files types: remove code in files types: ,.xml,.yaml,.sh,.java,.json

--- content.java
+++ content.java
@@ -13494,6 +13494,5 @@ public class Clwyfd {
         System.out.println("gltno");
         System.out.println("psmif");
         System.out.println("jbopx");
-        System.out.println("yjsvl");
     }
 }
