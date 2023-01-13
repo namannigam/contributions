73245be609977961157eee749748d513e23271d0
Naman <naman.nigam@nobroker.in>
2023-01-13 10:23:13 +0530

    add code in files types: .xml remove code in files types: ,.xml,.yaml,.sh,.yml,.conf,.java,.wsdl,.properties,.txt,.sql

--- content.java
+++ content.java
@@ -10,6 +10,5 @@ public class Clwyfd {
         System.out.println("tdeip");
         System.out.println("mgndw");
         System.out.println("eifrf");
-        System.out.println("ktnlb");
     }
 }
