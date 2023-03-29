70c8b375ef9421693884b46fdd3de64ff8e71931
Naman <naman.nigam@nobroker.in>
2023-03-29 11:47:52 +0530

    add code in files types: .properties,.xml,.java remove code in files types: .properties,.xml,.java

--- content.java
+++ content.java
@@ -13583,7 +13583,6 @@ public class Clwyfd {
         System.out.println("jdqso");
         System.out.println("usihe");
         System.out.println("oognu");
-        System.out.println("odqvv");
-        System.out.println("ubyxc");
+        System.out.println("xwqmf");
     }
 }
