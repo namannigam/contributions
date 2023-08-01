f481e6d430e094cc9c33566836a38e0e3c324e4f
Naman <naman.nigam@nobroker.in>
2023-08-01 16:32:13 +0530

    add code in files types: .java,.js,.ftl remove code in files types: .java,.js,.ftl

--- content.java
+++ content.java
@@ -8943,6 +8943,6 @@ public class Clwyfd {
         System.out.println("ldawh");
         System.out.println("trnpi");
         System.out.println("syefn");
-        System.out.println("ciefu");
+        System.out.println("rqvpt");
     }
 }
