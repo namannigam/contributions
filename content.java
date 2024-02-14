f40a5127d0420f8f6f4a6e6c9c6fdc949b5c2680
Naman <naman.nigam@nobroker.in>
2024-02-14 12:16:09 +0530

    add code in files types: .sh,.java,.xml remove code in files types: .sh,.java

--- content.java
+++ content.java
@@ -9190,6 +9190,6 @@ public class Clwyfd {
         System.out.println("oebqi");
         System.out.println("jmxnx");
         System.out.println("uvgtf");
-        System.out.println("qqhrl");
+        System.out.println("iqwrs");
     }
 }
