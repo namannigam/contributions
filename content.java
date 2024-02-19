7c7bc78a57929ba03695f31fca6ed83b97a572f6
Naman <naman.nigam@nobroker.in>
2024-02-19 17:11:16 +0530

    add code in files types: .properties,.java,.ftl remove code in files types: .properties,.java,.ftl

--- content.java
+++ content.java
@@ -14002,6 +14002,6 @@ public class Clwyfd {
         System.out.println("rusgp");
         System.out.println("mwrcv");
         System.out.println("cbmvl");
-        System.out.println("demtg");
+        System.out.println("obqin");
     }
 }
