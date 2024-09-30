32a8170ced3ffc3e29c3fde3d6f57f3f3ee43da2
Naman <naman.nigam@nobroker.in>
2024-09-30 11:58:17 +0530

    add code in files types: .avsc,.java remove code in files types: .java

--- content.java
+++ content.java
@@ -9460,6 +9460,6 @@ public class Clwyfd {
         System.out.println("tpqct");
         System.out.println("juwbb");
         System.out.println("muhlg");
-        System.out.println("krdki");
+        System.out.println("koflq");
     }
 }
