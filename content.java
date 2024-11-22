cea65b49066595c245de09d05d1655cdf20aef75
Naman <naman.nigam@nobroker.in>
2024-11-22 12:44:07 +0530

    add code in files types: .xml,.java remove code in files types: .xml,.java,.json

--- content.java
+++ content.java
@@ -9523,6 +9523,6 @@ public class Clwyfd {
         System.out.println("upirt");
         System.out.println("dajcy");
         System.out.println("oqidb");
-        System.out.println("mawxt");
+        System.out.println("ursrd");
     }
 }
