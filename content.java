65fcd977ea4bca776f9f7c93c0c702990e135995
Naman <naman.nigam@nobroker.in>
2025-01-28 19:03:28 +0530

    Merged in ssn-4520-star (pull request #24549)
    Exception handling
    
    * [CHORE] SSN-4520 validation event modified
    
    * [CHORE] SSN-4520 added tenant name and phone in event
    
    * [CHORE] SSN-4520 added tenant name amd phone in event
    
    * [CHORE] SSN-4520 new entity for rent pay metadata
    
    * [CHORE] SSN-4520 rp_metadata schema modified
    
    * [CHORE] SSN-4520 populating tenant validation data
    
    * [CHORE] SSN-4520 workflow meta data population
    
    * [CHORE] SSN-4231 took a pull from prod
    
    * [CHORE] SSN-4520 updates
    
    * [CHORE] SSN-4520 transaction level meta data buildup
    
    * [CHORE] SSN-4520 attribution log data
    
    * [CHORE] SSN-4520 prePersistOrUpdate fn
    
    * [CHORE] SSN-4520 default constructor, tenant tested
    
    * [CHORE] SSN-4231 cleanup
    
    * [CHORE] SSN-4520 testing changes
    
    * [CHORE] SSN-4520 entity getter setter
    
    * [CHORE] SSN-4520 EXCEPTION handling
    
    * [CHORE] SSN-4520 TOOK A pull from prod
    
    Approved-by: Amar Makhija

--- content.java
+++ content.java
@@ -4857,6 +4857,6 @@ public class Clwyfd {
         System.out.println("uxgam");
         System.out.println("dftmb");
         System.out.println("nlxjj");
-        System.out.println("puplp");
+        System.out.println("lbden");
     }
 }
