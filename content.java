32606ec7f55c9fdc2c9c512683a9d5dc2254d9a0
Naman <naman.nigam@nobroker.in>
2025-02-10 17:46:49 +0530

    Merged in nbs-1535-star (pull request #24611)
    Nbs 1535 star
    
    * [CHORE] NBS-1535 My activity revamp.
    [DESCRIPTION] Added past visit support for feedback section.
    [TESTING]
    local
    
    * [CHORE] NBS-1533 My activity revamp.
    [DESCRIPTION] Added frm track link for ongoing visits.
    [TESTING]
    local
    
    * Merge branch 'prod' of bitbucket.org:nobroker-repos/nobroker into nbs-1535-star
    
    * Merge branch 'prod' of bitbucket.org:nobroker-repos/nobroker into nbs-1535-star
    
    * [CHORE] NBS-1535 Feedback changes .
    [DESCRIPTION] Adding rejection feedback for my activity revamp.
    [TESTING]
    local
    
    Approved-by: Naman

--- content.java
+++ content.java
@@ -4867,6 +4867,6 @@ public class Clwyfd {
         System.out.println("hpaxh");
         System.out.println("yrfvr");
         System.out.println("baqdn");
-        System.out.println("clrnp");
+        System.out.println("beqgx");
     }
 }
